3bf437cf91211bd2f6577fa94a88f5040e826a6e
Sharad Shinde <sharad.shinde@coditas.com>
2020-11-06 19:09:08 +0530

    Update: Date Filter

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-toolbar',
-  templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+	selector: 'app-toolbar',
+	templateUrl: './toolbar.component.html',
+	styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
+	startDate: any;
+	endDate: any;
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
+	onDateChange(event): void {
+		console.log(this.startDate);
+		console.log(this.endDate);
+	}
+
+	clearDate(): void {
+		this.startDate = null;
+		this.endDate = null;
+	}
 }
